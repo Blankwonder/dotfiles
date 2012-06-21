6cdd5225282d1cc57660751c77528f39fbc0028f
Blankwonder <L@Yach.me>
2012-06-21 13:00:17 +0800

    add link script

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf `realpath .vimrc` ~/.vimrc
+ln -sf `realpath .bash_aliases` ~/.bash_aliases
+ln -sf `realpath .bash_profile` ~/.bash_profile
+ln -sf `realpath .bashrc` ~/.bashrc
+ln -sf `realpath .multitailrc` ~/.multitailrc
+ln -sf `realpath .screenrc` ~/.screenrc
