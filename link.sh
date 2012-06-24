7878d39eae615e291c6804ca11051bd16ce47acf
Blankwonder <L@Yach.me>
2012-06-25 00:12:43 +0800

    remove .bash_alias

--- link.sh
+++ link.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ln -sf `realpath .vimrc` ~/.vimrc
-ln -sf `realpath .bash_aliases` ~/.bash_aliases
 ln -sf `realpath .bash_profile` ~/.bash_profile
 ln -sf `realpath .bashrc` ~/.bashrc
 ln -sf `realpath .multitailrc` ~/.multitailrc
