21fe5b8e93abad01cecfd2801d33a0bef1060af0
Blankwonder <L@Yach.me>
2012-06-25 21:07:57 +0800

    update link.sh

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DIR=`dirname $0`
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+
 ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $DIR/.bash_profile ~/.bash_profile
 ln -sf $DIR/.bashrc ~/.bashrc
