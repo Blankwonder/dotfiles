e5f8b11e1f51d6a8fb106e932ac0e6674c43ecf7
Blankwonder <l@yach.me>
2013-03-02 14:04:14 +0800

    readd .bash_profile

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+ln -sfn $DIR/.bash_profile ~/.bash_profile
 ln -sfn $DIR/.vimrc ~/.vimrc
 ln -sfn $DIR/.bashrc ~/.bashrc
 ln -sfn $DIR/.multitailrc ~/.multitailrc
